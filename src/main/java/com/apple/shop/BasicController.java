fb78ea5e97f7036d548d7d933e21c54b610237da
leejinuk <leejinouk123@kakao.com>
2024-03-29 13:41:01 +0900

    first commit

---
+++ src/main/java/com/apple/shop/BasicController.java
@@ -0,0 +1,32 @@
+package com.apple.shop;
+
+import java.time.LocalDateTime;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller //자동완성하면 위에 import가 자동으로 된다.
+//아무 클래스에 Controller만 붙이면 main함수에서 가져와준다.
+public class BasicController {
+    @GetMapping("/") //메인페이지로 접속하면
+    //@ResponseBody 이걸 붙이면 문자데이터를 보내달라는 뜻이 된다. 빼면 html파일이 그대로 간다.
+    String hello(){ //함수 실행
+        return "index.html";
+    }
+
+    @GetMapping("/about") //메인페이지로 접속하면
+    @ResponseBody
+    String about(){ //함수 실행
+        return "이진욱입니다.";
+    }
+
+    @GetMapping("/date") //메인페이지로 접속하면
+    @ResponseBody
+    String date(){ //함수 실행
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        System.out.println("현재 날짜와 시간: " + currentDateTime);
+        return "현재 날짜와 시간:" + currentDateTime;
+
+    }
+
+}
---
+++ src/main/java/com/apple/shop/ItemController.java
@@ -0,0 +1,16 @@
+package com.apple.shop; //이 클래스를 다른 파일에서도 쓰고싶으면
+//경로를 패키지로 적어둬야한다.
+//com/apple/shop/ItemController.java
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ItemController {
+
+    @GetMapping("/list")
+    String list(){
+        return "list.html";
+    }
+
+}
