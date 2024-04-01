916452994541027c06acb2ba1348171b2bba1d7a
leejinuk <leejinouk123@kakao.com>
2024-04-01 22:42:22 +0900

    detail.html을 추가해보았다.

--- src/main/java/com/apple/shop/BasicController.java
+++ src/main/java/com/apple/shop/BasicController.java
@@ -1,9 +1,10 @@
 package com.apple.shop;
 
-import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.time.LocalDateTime;
 
 @Controller //자동완성하면 위에 import가 자동으로 된다.
 //아무 클래스에 Controller만 붙이면 main함수에서 가져와준다.
@@ -14,28 +15,33 @@ public class BasicController {
         return "index.html";
     }
 
-    @GetMapping("/about") //메인페이지로 접속하면
-    @ResponseBody
-    String about(){ //함수 실행
-        return "이진욱입니다." +
-                "<a href=\"http://localhost:8080\"><p>index로 가기</p> </a>";
+    @GetMapping("/detail")
+    String detail(){
+        return "detail.html";
     }
 
-    @GetMapping("/github") //메인페이지로 접속하면
-    @ResponseBody
-    String githublink(){ //함수 실행
-        return "https://github.com/Leejinuk123/SpringBoot" +
-                "<a href=\"http://localhost:8080\"><p>index로 가기</p> </a>";
-    }
+//    @GetMapping("/about") //메인페이지로 접속하면
+//    @ResponseBody
+//    String about(){ //함수 실행
+//        return "이진욱입니다." +
+//                "<a href=\"http://localhost:8080\"><p>index로 가기</p> </a>";
+//    }
+//
+//    @GetMapping("/github") //메인페이지로 접속하면
+//    @ResponseBody
+//    String githublink(){ //함수 실행
+//        return "https://github.com/Leejinuk123/SpringBoot" +
+//                "<a href=\"http://localhost:8080\"><p>index로 가기</p> </a>";
+//    }
+//
+//    @GetMapping("/date") //메인페이지로 접속하면
+//    @ResponseBody
+//    String date(){ //함수 실행
+//        LocalDateTime currentDateTime = LocalDateTime.now();
+//        System.out.println("현재 날짜와 시간: " + currentDateTime);
+//        return "현재 날짜와 시간:" + currentDateTime + " " +
+//                "<a href=\"http://localhost:8080\"><p>index로 가기</p> </a>";
 
-    @GetMapping("/date") //메인페이지로 접속하면
-    @ResponseBody
-    String date(){ //함수 실행
-        LocalDateTime currentDateTime = LocalDateTime.now();
-        System.out.println("현재 날짜와 시간: " + currentDateTime);
-        return "현재 날짜와 시간:" + currentDateTime + " " +
-                "<a href=\"http://localhost:8080\"><p>index로 가기</p> </a>";
-
-    }
+//    }
 
 }
